48ceb7201ae1cb409eb565fb5f7309d428dcfb8a
Harsh-sd <harshsandilya05042002@gmail.com>
2024-03-19 19:49:27 +0530

    postfix evaluation

---
+++ postfixEvaluation.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+// initialized the stack
+stack<int> st;
+// this will return the result
+int postfix(string s)
+{
+    // In case of prefix traverse from last
+    for (int i=0;i<s.length();i++)
+    {
+        // this is done for the purpose of ascii value
+        if (s[i] >= '0' && s[i] <= '9')
+        {
+            st.push(s[i] - '0');
+        }
+        else
+        {
+
+            int op2 = st.top();
+            st.pop();
+            int op1 = st.top();
+            st.pop();
+            switch (s[i])
+            {
+            case '+':
+                st.push(op1 + op2);
+                break;
+            case '-':
+                st.push(op1 - op2);
+                break;
+            case '*':
+                st.push(op1 * op2);
+                break;
+            case '/':
+                st.push(op1 / op2);
+                break;
+            case '^':
+                st.push(op1 ^ op2);
+                break;
+
+            default:
+                break;
+            }
+        }
+    }
+    return st.top();
+}
+int main()
+{
+    string s = "46+2/5*7+";
+    cout << postfix(s);
+}
