041725dec1c9bed21cbd47ef3a3c0520d0999cf1
Harsh-sd <harshsandilya05042002@gmail.com>
2024-03-20 10:48:00 +0530

    conversion of infix to prefix

---
+++ infixToPrefix.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <stack>
+// To reverse the string
+#include <algorithm>
+using namespace std;
+stack<char> st;
+string res = "";
+int prec(char c)
+{
+    if (c == '^')
+    {
+        return 3;
+    }
+    else if (c == '*' || c == '/')
+    {
+        return 2;
+    }
+    else if (c == '+' || c == '-')
+    {
+        return 1;
+    }
+    else
+    {
+        return -1;
+    }
+}
+string infixtoprefix(string s)
+{
+    reverse(s.begin(), s.end());
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] >= 'a' && s[i] <= 'z' || s[i] >= 'A' && s[i] <= 'Z')
+        {
+            res += s[i];
+        }
+        // In case of prefix change the direction of brackets
+        else if (s[i] == ')')
+        {
+            st.push(s[i]);
+        }
+        else if (s[i] == '(')
+        {
+            while (!st.empty() && st.top() != ')')
+            {
+                res += st.top();
+                st.pop();
+            };
+            if (!st.empty() && st.top() == ')')
+            {
+                st.pop();
+            }
+        }
+        else
+        {
+            while (!st.empty() && prec(st.top()) >= prec(s[i]))
+            {
+                res += st.top();
+                st.pop();
+            }
+            st.push(s[i]);
+        }
+    }
+    while (!st.empty())
+    {
+        res += st.top();
+        st.pop();
+    }
+    return res;
+}
+int main()
+{
+    string s = "(a-b/c)*(a/k-l)";
+    cout << infixtoprefix(s);
+}
